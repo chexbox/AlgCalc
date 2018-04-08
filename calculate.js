dc7125f2e19ecec629030fedd989dabb7875a520
chexbox <chexbox@mailinator.com>
2018-04-08 15:43:11 -0500

    Center using css

--- calculate.js
+++ calculate.js
@@ -9,4 +9,25 @@ function clr() {
   v.value = "";
 };
 
+function calculate(what) {
+  var cnst[];
+  var oper[];
+  var i = 0;
+  var j;
+  var pre;
+  var order;
+  while (i <= what.length) {
+    if (typeof what[i] == 'number') {
+      if (pre !== 'number') {
+        oper.push(what[i]);
+        pre = 
+      } else {
+        j += 1;
+      };
+    };
+    i += 1
+    
+  };
+};
+
 document.getElementById("in").value = "";
