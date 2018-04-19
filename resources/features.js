1a271049d65c32096aa7c8bf337d4f9e776db5ca
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-19 18:48:07 -0500

    Add settings menu support

--- resources/features.js
+++ resources/features.js
@@ -3,11 +3,24 @@ function menutoggle() {
     'use strict';
     document.getElementById("drop").classList.toggle("show");
     document.getElementById("mainbar").classList.toggle("fixed");
-}
+};
+function settingstoggle() {
+    'use strict';
+    document.getElementById("settings").classList.toggle("show");
+    document.getElementById("mainbar").classList.toggle("fixed");
+};
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     'use strict';
     if (!(event.target == document.getElementById("drop")) && !(event.target == document.getElementById("dropbtn"))) {
+
+        document.getElementById("drop").classList.remove("show");
+        document.getElementById("mainbar").classList.remove("fixed");
+    }
+}
+window.onclick = function (event) {
+    'use strict';
+    if (!(event.target == document.getElementById("settings")) && !(event.target == document.getElementById("settingsbtn"))) {
 
         document.getElementById("drop").classList.remove("show");
         document.getElementById("mainbar").classList.remove("fixed");
