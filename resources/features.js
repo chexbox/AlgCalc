b9cdd8243c9740c373bc5060a6574c999cb218e4
chexbox <31783961+chexbox@users.noreply.github.com>
2018-04-20 08:44:17 -0500

    Update features.js

--- resources/features.js
+++ resources/features.js
@@ -12,16 +12,9 @@ function settingstoggle() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     'use strict';
-    if (!(event.target == document.getElementById("drop")) && !(event.target == document.getElementById("dropbtn"))) {
+    if (!(event.target == document.getElementById("drop")) && !(event.target == document.getElementById("dropbtn")) && !(event.target == document.getElementById("settings")) && !(event.target == document.getElementById("settingsbtn"))) {
 
         document.getElementById("drop").classList.remove("show");
-        document.getElementById("mainbar").classList.remove("fixed");
-    }
-}
-window.onclick = function (event) {
-    'use strict';
-    if (!(event.target == document.getElementById("settings")) && !(event.target == document.getElementById("settingsbtn"))) {
-
         document.getElementById("settings").classList.remove("show");
         document.getElementById("mainbar").classList.remove("fixed");
     }
