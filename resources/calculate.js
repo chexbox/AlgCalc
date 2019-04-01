58dc3666bbd929df7e94ecb05a4baa4fb507dc67
chexbox <ethan.g.s@outlook.com>
2019-04-01 18:13:54 -0500

    Update calculate.js to use html
    may break <input> calculators

--- resources/calculate.js
+++ resources/calculate.js
@@ -12,60 +12,71 @@ function append(char) {
 function clr() {
   var v = document.getElementById("in");
   v.value = "";
+  v.innerHTML = "";
 };
 
 function calc() {
   var v = document.getElementById("in");
   var res = eval(v.value);
   v.value = res;
+  v.innerHTML = res;
 };
 
 function del() {
   var v = document.getElementById("in");
   res = v.value.substring(0, v.value.length - 1);
   v.value = res;
+  v.innerHTML = res;
 };
 
 function sqr() {
   var v = document.getElementById("in");
   res = eval(v.value) * eval(v.value);
   v.value = res;
+  v.innerHTML = res;
 };
 
 function sqrt() {
   var v = document.getElementById("in");
   res = Math.sqrt(eval(v.value));
   v.value = res;
+  v.innerHTML = res;
 };
 
 function abs() {
   var v = document.getElementById("in");
   res = Math.abs(eval(v.value));
   v.value = res;
+  v.innerHTML = res;
 };
 function pct() {
   var v = document.getElementById("in");
   res = ( eval(v.value) * 100 );
   v.value = res;
+  v.innerHTML = res;
 };
 function sine() {
   var v = document.getElementById("in");
   res = Math.sin(eval(v.value));
   v.value = res;
+  v.innerHTML = res;
 };
 function cosi() {
   var v = document.getElementById("in");
   res = Math.cos(eval(v.value));
   v.value = res;
+  v.innerHTML = res;
 };
 function tang() {
   var v = document.getElementById("in");
   res = Math.tan(eval(v.value));
   v.value = res;
+  v.innerHTML = res;
 };
 function logr() {
   var v = document.getElementById("in");
   v.value = eval(v.value)
-  res = Math.log(v.value);
+  res = Math.log(res);
   v.value = res;
+  v.innerHTML = res;
 };
